714ed05a5e4d4dee5a33b47acdcc77b18561585d
Lucas Scolaro <lucas.scolaro@ifsc.edu.br>
2022-04-18 20:37:45 -0300

    Resolução da Tarefa 167

--- app/Models/Venda.php
+++ app/Models/Venda.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Cliente;
 
 class Venda extends Model
 {
     use HasFactory;
     protected $table = "vendas";
+
+    function cliente(){
+        return $this->belongsTo(Cliente::class, 
+        'id_cliente', 'id');
+    }
 }
